dadef71084af5e6615a6fd373a5e404af84b4576
anthony-alepra <Anthony.alepra@gmail.com>
2022-06-27 18:12:48 +0000

    integer_birth_year works

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -1,9 +1,15 @@
 # Ask for a person’s age, and then calculate and display what year they were born in.
-# 
+#
 # Input:
 #   80
-# 
+#
 # Key output:
 #   "Wow, you were born in 1940. You're old!"
 
-p "How old are you?"
+puts "How old are you?"
+
+age = gets.to_i
+
+year = 2022 - age
+
+print "Wow, you were born in " + year.to_s + ". You're old!"
